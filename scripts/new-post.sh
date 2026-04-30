10edeec6f52066e43b5d84f6a1d3a071f395abc3
legoraft <git@legoraft.com>
2026-04-30 15:42:07 +0200

    feat: updated UI for new post

--- scripts/new-post.sh
+++ scripts/new-post.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-echo """-- New post --
-"""
+printf "\t%s\n\n" "-- New post --"
 
-read -rp "Title: " title
+read -rp "$(printf "\e[96mTitle\e[0m: ")" title
 
 generated_slug=$(echo $title |
     tr "[:upper:]" "[:lower:]" |
     sed -E "s/[^a-z0-9]/-/g; s/^-+|-+$//g; s/-+/-/g")
 
-read -rp "Slug [${generated_slug}]: " slug
+read -rp "$(printf "\e[96mSlug \e[0m[\e[90m${generated_slug}\e[0m]: ")" slug
 slug=${slug:-$generated_slug}
 
 generated_branch="post/${slug}"
 file="content/posts/${slug}.md"
 
-read -rp "Branch [${generated_branch}]: " branch
+read -rp "$(printf "\e[96mBranch \e[0m[\e[90m${generated_branch}\e[0m]: ")" branch
 branch=${branch:-$generated_branch}
 
-git checkout -b "$branch"
+git checkout -b "$branch" -q
+printf "\n\e[32m  ✓ checkout\e[0m %s \n" "$branch"
+
 mkdir -p $(dirname "$file")
 
 cat << EOF > $file
@@ -32,3 +33,5 @@ updated = $(date +%Y-%m-%d)
 tags = [  ]
 +++
 EOF
+
+printf "\e[32m  ✓ create\e[0m %s \n\n" "$file"
