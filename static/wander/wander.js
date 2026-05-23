fb7de62910fd4d54451b9c5721dd4cd80cfce9e4
legoraft <git@legoraft.com>
2026-05-23 13:01:50 +0200

    feat: added wander console

---
+++ static/wander/wander.js
@@ -0,0 +1,21 @@
+const wander = {
+  // Other Wander consoles that visitors can reach from my console.
+  consoles: [
+    "https://susam.net/wander/",
+  ],
+
+  // My favourite websites and pages I recommend to the Wander community.
+  pages: [
+    "https://matduggan.com",
+    "https://overreacted.io",
+    "https://sandboxspirit.com",
+    "https://blog.vaxry.net"
+  ],
+
+  // Websites and consoles to ignore.  My console will never fetch
+  // consoles or web pages whose URLs match the following patterns.
+  ignore: [
+    // Fake examples, please replace with actual URLs.
+    "https://joshing.you/wander",
+  ],
+}
